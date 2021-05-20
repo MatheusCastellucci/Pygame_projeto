1d7eee9efd914f530fd30f56bcbc2c44211d74cc
MatheusCastel <matranec@gmail>
2021-05-20 09:08:45 -0300

    Cria tela - beta

---
+++ jogo.py
@@ -0,0 +1,28 @@
+import pygame
+
+White = (255, 255, 255)
+Red = (255, 0, 0)
+Black= (0, 0, 0)
+Green = (0, 255, 0)
+Gold = (255, 215, 0)
+Blue = (0, 0, 250)
+
+
+pygame.init()
+
+window = pygame.display.set_mode((800, 800))
+pygame.display.set_caption('Professors Invasion!!')
+
+game = True
+
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or event.type == pygame.KEYUP:
+            game = False
+
+    window.fill((0, 0, 0))
+
+    pygame.display.update()
+
+
+pygame.quit()
