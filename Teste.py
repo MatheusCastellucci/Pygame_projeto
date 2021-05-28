db91bc0f3795dfcda82d64324428a4ed91249a48
Juuzou623 <pedrohois@al.insper.edu.br>
2021-05-28 10:40:55 -0300

    Adicionando 2 inimigos com imagens provisórias

--- Teste.py
+++ Teste.py
@@ -6,14 +6,18 @@ pygame.init()
 
 width = 600
 height = 600
-HUMBERTO_WIDTH = 50
-HUMBERTO_HEIGHT = 38
+PROFESSOR_WIDTH = 50
+PROFESSOR_HEIGHT = 38
 NAVE_WIDTH = 50
 NAVE_HEIGHT = 35
 background = pygame.image.load('imagens/back.png')
 background = pygame.transform.scale(background, (width, height))
 humberto_img = pygame.image.load('imagens/Ft_Humberto.png')
-humberto_img = pygame.transform.scale(humberto_img, (HUMBERTO_WIDTH, HUMBERTO_HEIGHT))
+humberto_img = pygame.transform.scale(humberto_img, (PROFESSOR_WIDTH, PROFESSOR_HEIGHT))
+hage_img = pygame.image.load('imagens/nave.png')
+hage_img = pygame.transform.scale(hage_img, (PROFESSOR_WIDTH, PROFESSOR_HEIGHT))
+guzzo_img = pygame.image.load('imagens/nave.png')
+guzzo_img = pygame.transform.scale(guzzo_img, (PROFESSOR_WIDTH, PROFESSOR_HEIGHT))
 nave_img = pygame.image.load('imagens/nave.png')
 nave_img = pygame.transform.scale(nave_img, (NAVE_WIDTH, NAVE_HEIGHT))
 
@@ -30,7 +34,7 @@ class Player:
         playerImg = nave_img
         screen.blit(playerImg, (self.x, self.y))
 
-class Enemy:
+class EnemyHUM:
     def __init__(self, x, y):
         self.x = x
         self.y = y - 200
@@ -49,7 +53,43 @@ class Enemy:
                 lasers.remove(laser)
                 enemies.remove(self)
 
+class EnemyGUZZO:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y - 200
 
+    def draw(self):
+        enemyImg2 = guzzo_img 
+        screen.blit(enemyImg2, (self.x, self.y))
+        self.y += 0.81
+
+    def detectCollision(self):
+        for laser in lasers:
+            if (laser.x > self.x and
+                    laser.x < self.x + 50 and
+                    laser.y > self.y and
+                    laser.y < self.y + 50):
+                lasers.remove(laser)
+                enemies.remove(self)
+
+class EnemyHAGE:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y - 200
+
+    def draw(self):
+        enemyImg3 = hage_img
+        screen.blit(enemyImg3, (self.x, self.y))
+        self.y += 0.81
+
+    def detectCollision(self):
+        for laser in lasers:
+            if (laser.x > self.x and
+                    laser.x < self.x + 50 and
+                    laser.y > self.y and
+                    laser.y < self.y + 50):
+                lasers.remove(laser)
+                enemies.remove(self)
 class Laser:
     def __init__(self, x, y):
         self.x = x + 25
@@ -71,8 +111,9 @@ lasers = []
 
 # Spawn enemies
 for x in range(1, 10):
-    for y in range(1, 4):
-        enemies.append(Enemy(x * 55, y * 50))
+    enemies.append(EnemyHUM(x * 55, 50))
+    enemies.append(EnemyHAGE(x * 55, 100))
+    enemies.append(EnemyGUZZO(x * 55, 150))
 
 def displayText(text):
     font = pygame.font.SysFont('', 50)
@@ -114,7 +155,7 @@ while isPlaying:
         lasers = []
         for x in range(1, 10):
             for y in range(1, 4):
-                enemies.append(Enemy(x * 55, y * 50))
+                enemies.append(EnemyHUM(x * 55, y * 50))
         enemy.draw()
         enemy.detectCollision()
         if enemy.y > height-20:
